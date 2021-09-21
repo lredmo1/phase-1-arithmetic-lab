fb2ab7975d444ec559e1f8b17c40fe634a13eca5
Lisa <redlisa125@gmail.com>
2021-09-20 15:24:44 -1000

    commit tree 62731df3d8602875c54d92c3d32cd725a483b90b

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+
+
+const random = Math.ceil(Math.random() * 10);
+
+let num3 = 22;
+let num4 = 6;
+const mod = num3 % num4;
+
+
+
+const max = Math.max(20, 5, 6, 7, 8);
